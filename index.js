d255446f9a6b00e4b59ee808addc2a4d027077d6
Samson Tmg <stmg202023@gmail.com>
2023-03-14 19:56:51 +0545

    express js server, diff method for one path, res/req method, next(), middleware for all/specific, req.query, auth

---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
+
+// console.log(data);
+// console.log(index);
+// console.log(typeof products);
+
+const express = require("express");
+const { Server } = require("http");
+const app = express();
+
+//======MiddleWare=====
+
+//Middleware used for whole application
+// app.use((req, res) => {
+//   console.log(
+//     req.method,
+//     req.ip,
+//     req.hostname,
+//     new Date(),
+//     req.get("User-Agent")
+//   );
+
+//   req.next();
+// });
+
+const auth = (req, res, next) => {
+  console.log(req.query); // req.query check the path // output: {} //as it is empty
+  // send http://localhost:8080/?password and reload // output: { password: ""}
+  // send http://localhost:8080/?password=123 and reload // output: { password: "123"} // 123
+  console.log(req.query.password);
+
+  //if http://localhost:8080/?password=123 then next() // do not put any password for auth and use ==
+  if (req.query.password === "123") {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+  //   next();
+};
+
+// It used for all method(GET, PUT, POST...)
+// app.use(auth);
+
+//API -Endpoint  -Route
+
+//using auth for GET method
+app.get("/", auth, (req, res) => {
+  res.json({ type: "GET" });
+});
+
+//using auth for POST method
+app.post("/", auth, (req, res) => {
+  res.json({ type: "POST" });
+});
+
+app.put("/", (req, res) => {
+  res.json({ type: "PUT" });
+});
+
+app.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+
+app.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
+
+app.get("/demo", (req, res) => {
+  //   res.sendStatus(404);
+  //   res.send("<h1>Hello World</h1>");
+  //   res.status(201).send("<h1>Hello World</h1>");
+  //   res.json(products);
+});
+
+app.listen(8080, () => {
+  console.log("server started");
+});
